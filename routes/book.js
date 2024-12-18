419fb429e18dcd0056618b61dfb80825bbc0faf3
noscatis <michael.bocquel@gmail.com>
2024-12-18 13:39:34 +0100

    feat: add auth and multer for routes that needs it

--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestRatedBooks);
-router.post("/", bookCtrl.postOneBook);
-router.put("/:id", bookCtrl.updateOneBook);
-router.delete("/:id", bookCtrl.deleteOneBook);
-router.post("/:id/rating", bookCtrl.rateOneBook);
+router.post("/", auth, multer, bookCtrl.postOneBook);
+router.put("/:id", auth, multer, bookCtrl.updateOneBook);
+router.delete("/:id", auth, multer, bookCtrl.deleteOneBook);
+router.post("/:id/rating", auth, bookCtrl.rateOneBook);
 
 module.exports = router;
