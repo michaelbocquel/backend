2aa0fad3b8deb55ade934d87de84ef28988a851e
noscatis <michael.bocquel@gmail.com>
2024-12-30 13:53:46 +0100

    feat: swap route order to have bestrating work properly

--- routes/book.js
+++ routes/book.js
@@ -6,9 +6,9 @@ const multer = require("../middleware/multer-config");
 const bookCtrl = require("../controllers/book");
 
 router.get("/", bookCtrl.getAllBooks);
-router.get("/:id", bookCtrl.getOneBook);
-router.get("/bestrating", bookCtrl.getBestRatedBooks);
 router.post("/", auth, multer, bookCtrl.postOneBook);
+router.get("/bestrating", bookCtrl.getBestRatedBooks);
+router.get("/:id", bookCtrl.getOneBook);
 router.put("/:id", auth, multer, bookCtrl.updateOneBook);
 router.delete("/:id", auth, multer, bookCtrl.deleteOneBook);
 router.post("/:id/rating", auth, bookCtrl.rateOneBook);
